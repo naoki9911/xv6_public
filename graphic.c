8f13f7bc7abb8d480728a56fbc96e769846f03f0
Naoki Matsumoto <m.naoki9911@gmail.com>
2018-08-20 23:45:56 +0900

    draw boot log

--- graphic.c
+++ graphic.c
@@ -2,18 +2,24 @@
 #include "mp_uefi.h"
 #include "memlayout.h"
 
+/*
+ * i%4 = 0 : blue
+ * i%4 = 1 : green
+ * i%4 = 2 : red
+ * i%4 = 3 : black
+ */
 struct graphic_config graphic_conf;
 unsigned char *frame;
 void graphic_init(){
-  frame = (unsigned char *)(DEVSPACE - VRAMSIZE);
+  frame = (unsigned char *)(VRAMBASE_MAPPED);
   
-  for(long i=0;i<VRAMSIZE;i++){
-    if(i%3 == 1){
+  /*for(long i=0;i<VRAMSIZE;i++){
+    if(i%4 != 3){
       *(frame+i) = 0xFF;
     }else{
       *(frame+i) = 0x00;
     }
-  }
+  }*/
 }
 
 void graphic_draw_box(){
@@ -21,7 +27,10 @@ void graphic_draw_box(){
 
 }
 
-void graphic_draw_pixel(int x,int y){
-
-
+void graphic_draw_pixel(int x,int y,struct graphic_pixel * buffer){
+  int pixel_addr = (sizeof(struct graphic_pixel))*(y*HORIZONTAL_PIXELS + x);
+  struct graphic_pixel *pixel = (struct graphic_pixel *)(VRAMBASE_MAPPED + pixel_addr);
+  pixel->blue = 0x00;
+  pixel->green = 0xFF;
+  pixel->red = 0xFF;
 }
--- graphic.h
+++ graphic.h
@@ -1,5 +1,7 @@
 #ifndef GRAPHIC_H_
 #define GRAPHIC_H_
+#include "types.h"
+#include "memlayout.h"
 
 struct graphic_config {
   long long frame_base;
@@ -9,11 +11,21 @@ struct graphic_config {
   long long pixels_per_scan_line;
 };
 
+struct graphic_pixel {
+  uchar blue;
+  uchar green;
+  uchar red;
+  uchar black;
+};
+
 void graphic_init();
 void graphic_draw_box();
-void graphic_draw_pixel(int x,int y);
+void graphic_draw_pixel(int x,int y,struct graphic_pixel *);
 
 #define VRAMBASE 0x80000000
 #define VRAMSIZE 0x1D5000
+#define VRAMBASE_MAPPED (DEVSPACE-VRAMSIZE)
+#define HORIZONTAL_PIXELS 800
+#define VERTICAL_PIXELS 600
 
 #endif
